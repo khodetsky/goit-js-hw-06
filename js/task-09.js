396a993e5d3bd31b6cd836739fa698125bef9fa1
khodetsky <hodyaya@gmail.com>
2022-05-18 00:56:23 +0300

    Update task-09.js
    done task-10

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorValue = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+
+const getColor = () => {
+document.body.style.backgroundColor = `${getRandomHexColor()}`;
+colorValue.textContent = `${getRandomHexColor()}`;
+}
+const onChangeColorButtonClick = changeColorButton.addEventListener('click', getColor);
