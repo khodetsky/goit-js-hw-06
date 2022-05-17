9a5cab88648e8837ff416a04d84147f257eb905b
khodetsky <hodyaya@gmail.com>
2022-05-18 00:34:12 +0300

    Update task-08.js
    done task-8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+     alert("Будь ласка, заповніть всі порожні поля");
+  } else {
+    console.log({email: email.value, password: password.value});
+    event.currentTarget.reset();
+    };
+}
