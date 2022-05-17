b84daed362deb1ee86823d9f1ea1b2f8012981d3
khodetsky <hodyaya@gmail.com>
2022-05-17 15:12:20 +0300

    Update task-02.js
    done task-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+const ingredientsListEl = ingredients.forEach(ingredient => {
+  const elementOfIngredientsList = document.createElement('li');
+  elementOfIngredientsList.textContent = `${ingredient}`;
+  elementOfIngredientsList.classList.add("item");
+  return ingredientsList.append(elementOfIngredientsList);
+});
